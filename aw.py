8634d2b9d92b420ddf564b5d30d7cb7b0119a1f7
mehithka <mehith.karacasia@gmail.com>
2025-03-06 17:35:37 +0500

    a2

--- aw.py
+++ aw.py
@@ -1,1 +1,33 @@
 from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    def move(self):
+        pass
+
+class human(Animal):
+    def move(self):
+        print('i can walk and run')
+
+class snake(Animal):
+    def move(self):
+        print(' i can crawl')
+
+class dog (Animal):
+    def move(self):
+        print(' i can bark')
+
+class Lion(Animal):
+    def move(self):
+        print(' i can roar')
+
+R = human()
+R.move()
+
+K = snake()
+K.move()
+
+R = dog()
+R.move()
+
+K=Lion()
+K.move 
